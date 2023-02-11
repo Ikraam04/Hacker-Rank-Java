2f060db0aef4321814e639c83ee46a2d10d241d5
taco <i.mahmood2@newcastle.ac.uk>
2023-02-11 19:23:51 +0000

    Updated version

--- src/IsPalindrome.java
+++ src/IsPalindrome.java
@@ -4,8 +4,9 @@ public class IsPalindrome {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		StringBuilder s1 = new StringBuilder(input.nextLine());
-		Boolean result = (s1.toString().equals(s1.reverse().toString())) ? Boolean.TRUE : Boolean.FALSE;
+		Boolean result = (s1.toString().equals(s1.reverse().toString()));
 		System.out.println(result);
+
 
 
 	}
